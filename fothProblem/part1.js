ca67751d5cba2871a85ef3c6f3f1e8fe095b6e39
Ramzi-Abidi <ramziinfo2001@gmail.com>
2023-12-07 13:41:20 +0100

    adding forthProblem/part1

---
+++ fothProblem/part1.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+
+const solve = () => {
+    let res = 0;
+
+    // Read the contents of the text file
+    fs.readFile("input.txt", "utf8", (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+
+        const cards = data.split("\n"); // Split text into lines
+        cards.forEach((line) => {
+            let count = 0;
+            let i = 0;
+
+            const arr = line.split(":")[1];
+            let winningCards = arr.split(" | ")[0].trim().split(" ");
+            let myCards = arr.split(" | ")[1].trim().split(" ");
+            winningCards = winningCards.filter((el) => el !== "" && el !== " ");
+            myCards = myCards.filter((el) => el !== "" && el !== " ");
+
+            myCards.forEach((el) => {
+                if (winningCards.indexOf(el) !== -1) {
+                    console.log("a", i);
+                    if (i === 0) {
+                        count++;
+                        i++;
+                    } else {
+                        count *= 2;
+                    }
+                }
+            });
+            console.log("count", count);
+            res += count;
+        });
+        console.log("res", res);
+    });
+};
+
+console.log(solve());
